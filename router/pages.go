e593a6f9f71db5343057aca5f89101fec0bae4af
VoxelPrismatic <voxelprismatic@pm.me>
2024-11-21 13:46:41 -0600

    add home page

--- router/pages.go
+++ router/pages.go
@@ -1,7 +1,9 @@
 package router
 
 import (
+	"nbientry/router/fail"
 	"nbientry/web/common"
+	"nbientry/web/pages"
 	"net/http"
 )
 
@@ -10,5 +12,5 @@ func HomeRouter(w http.ResponseWriter, r *http.Request, user common.User, path [
 		http.Redirect(w, r, "/user/login", http.StatusTemporaryRedirect)
 		return
 	}
-	// fail.Render(w, r, pages.Home(user))
+	fail.Render(w, r, pages.Home(user))
 }
